aceca1727a78c4f792f16c75aef9838a3f011b9d
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-10 17:13:49 +0100

    user app set-up

--- afpo_transport/settings.py
+++ afpo_transport/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'transport_app',
+    'users',
 ]
 
 MIDDLEWARE = [
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def login_user(request):
+	return HttpResponse("LOgin here")
