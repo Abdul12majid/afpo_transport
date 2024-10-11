65d241931425e142e919aa61c4d4fbc1fedd149c
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-11 16:08:43 +0100

    create account model

--- transport_app/models.py
+++ transport_app/models.py
@@ -3,13 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Role(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=False, blank=False)
 
     def __str__(self):
         return self.name
 
 
+class Account(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.owner.username
 
 class Ride(models.Model):
     rider = models.ForeignKey(User, on_delete=models.CASCADE)
