46f250b67cb911de8b7645de1ddabeccd4dcac98
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-11 16:24:00 +0100

    registered model

--- transport_app/admin.py
+++ transport_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Role, Account, Ride
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Role)
+admin.site.register(Ride)
